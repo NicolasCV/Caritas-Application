8b814f647d84895425815a00a305d25ee5400e69
A00826925 <73501467+A00826925@users.noreply.github.com>
2021-11-11 09:38:26 -0600

    current

--- app/src/main/java/com/example/caritas/MainActivity.kt
+++ app/src/main/java/com/example/caritas/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.caritas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.Serializable
+
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
