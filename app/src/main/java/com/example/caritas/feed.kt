1e0d864639f55c403b2558139bafbb8c8e47c29b
A00826925 <73501467+A00826925@users.noreply.github.com>
2021-11-11 18:15:14 -0600

    dia de hoy

--- app/src/main/java/com/example/caritas/feed.kt
+++ app/src/main/java/com/example/caritas/feed.kt
@@ -1,61 +1,43 @@
 package com.example.caritas
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import javax.xml.parsers.SAXParser
-import javax.xml.parsers.SAXParserFactory
-import org.xml.sax.InputSource
-import org.xml.sax.XMLReader
-import org.xml.sax.helpers.DefaultHandler
-import android.R
-
-import android.widget.TextView
-
-
-
+import com.prof.rssparser.Parser
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-
 class Feed : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    var rssResult: String = ""
-    lateinit var rss: TextView
+
+    //private lateinit var myContext: Context
+    private lateinit var parser: Parser
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        parser = Parser.Builder()
+            .context(requireContext())
+            .build()
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        //Definir variables
-        rss = view.findViewById(R.id.rss)
-        SAXParserFactory factory = SAXParserFactory.newInstance()
-        SAXParser saxParser = factory.newSAXParser()
+        //return inflater.inflate(R.layout.fragment_feed, container, false)
 
     }
 
+
+
+\
 }
