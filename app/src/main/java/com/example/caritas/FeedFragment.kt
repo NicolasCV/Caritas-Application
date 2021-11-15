01b724f8f27e4b8cdd3a70442a700a6ea526774a
A00826925 <73501467+A00826925@users.noreply.github.com>
2021-11-14 21:51:26 -0600

    working
    make buttons work

---
+++ app/src/main/java/com/example/caritas/FeedFragment.kt
@@ -0,0 +1,61 @@
+package com.example.caritas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import com.prof.rssparser.Parser
+import kotlinx.coroutines.launch
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class FeedFragment : Fragment() {
+
+    private lateinit var parser: Parser
+    private lateinit var title: TextView
+
+    private val url = "https://www.caritas.org.mx/feed"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        parser = Parser.Builder()
+            .context(requireContext())
+            //.charset(Charset.forName("ISO-8859-7"))
+            .cacheExpirationMillis(24L * 60L * 60L * 100L) // one day
+            .build()
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            try {
+                val channel = parser.getChannel(url)
+                //do something
+                title = view.findViewById(R.id.TextTitle)
+                title.text = channel.title
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/example/caritas/MainActivity.kt
+++ app/src/main/java/com/example/caritas/MainActivity.kt
@@ -2,15 +2,23 @@ package com.example.caritas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import com.prof.rssparser.Parser
 import java.io.Serializable
-
+import java.nio.charset.Charset
 
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
     }
+
+
+
 }
