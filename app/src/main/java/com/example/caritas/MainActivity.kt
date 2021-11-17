0672f44d7e28be8a934e3287ddeb22f98496d954
A00826925 <73501467+A00826925@users.noreply.github.com>
2021-11-17 11:03:48 -0600

    buttons

--- app/src/main/java/com/example/caritas/MainActivity.kt
+++ app/src/main/java/com/example/caritas/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.caritas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.prof.rssparser.Parser
 import java.io.Serializable
 import java.nio.charset.Charset
@@ -10,12 +14,19 @@ import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var returnBtn: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        returnBtn = findViewById(R.id.returnButton)
 
+        returnBtn.setOnClickListener{
+            supportFragmentManager.commit{
+                replace<MainMenu>(R.id.fragmentCont)
+            }
+        }
 
     }
 
